22baf57e37b23ca1ac3ba5e70b4cff3ba27f5dc7
Ubuntu <ubuntu@ip-172-31-34-140.ap-south-1.compute.internal>
2025-11-03 12:20:54 +0000

    removed the unnecssary backend

--- source/routes/api.ts
+++
@@ -1,6 +0,0 @@
-import express, { Router, Response, NextFunction } from "express";
-import { AppConfig } from "../config/constants";
-import ApiEndpoints from "./v1/api";
-const ApiEndpoint: Router = express.Router();
-ApiEndpoint.use(`${AppConfig.API_VERSION}`, ApiEndpoints);
-export default ApiEndpoint;
