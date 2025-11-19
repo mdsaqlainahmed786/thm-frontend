964de33c5a3a6a37a18e5718a34f28d1a14b1a0b
Saqlain <riyanahmed1703@gmail.com>
2025-11-19 16:46:34 +0530

    Added the localhost domain to the next js config

--- next.config.js
+++ next.config.js
@@ -2,5 +2,17 @@
 const nextConfig = {
 	typescript: { ignoreBuildErrors: true },
 	eslint: { ignoreDuringBuilds: true },
-};
-module.exports = nextConfig;
+	images: {
+	  domains: [
+		"randomuser.me",
+		"the-hotel-media-bucket.s3.ap-south-1.amazonaws.com",
+		"ec2-13-202-52-159.ap-south-1.compute.amazonaws.com",
+		"localhost",
+		"127.0.0.1",
+		"thehotelmedia.com",
+		"lh3.googleusercontent.com",
+	  ],
+	},
+  };
+  module.exports = nextConfig;
+  
