63c22d137326209a90b85196e386caf0c79f96ae
Ubuntu <ubuntu@ip-172-31-34-140.ap-south-1.compute.internal>
2025-11-20 08:46:57 +0000

    updated the config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-  images: {
-    domains: [
-      "randomuser.me",
-      "the-hotel-media-bucket.s3.ap-south-1.amazonaws.com",
-      "ec2-13-202-52-159.ap-south-1.compute.amazonaws.com",
-      "localhost",
-      "127.0.0.1",
-      "thehotelmedia.com",
-      "staging.thehotelmedia.com",
-      "api.thehotelmedia.com",
-      "lh3.googleusercontent.com",
-    ],
-  },
+
+	typescript: { ignoreBuildErrors: true },
+	eslint: { ignoreDuringBuilds: true },
+	images: {
+	  domains: [
+		"randomuser.me",
+		"the-hotel-media-bucket.s3.ap-south-1.amazonaws.com",
+		"ec2-13-202-52-159.ap-south-1.compute.amazonaws.com",
+		"localhost",
+		"127.0.0.1",
+		"thehotelmedia.com",
+		"staging.thehotelmedia.com",
+		"api.thehotelmedia.com",
+		"admin.thehotelmedia.com", 
+		"lh3.googleusercontent.com",
+	  ],
+	},
 };
 module.exports = nextConfig;
