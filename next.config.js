e0df32a5907889968ab701c77802547b9a69820a
Saqlain <riyanahmed1703@gmail.com>
2026-02-06 12:51:08 +0530

    chore: set outputFileTracingRoot in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	outputFileTracingRoot: __dirname,
 	typescript: { ignoreBuildErrors: true },
 	eslint: { ignoreDuringBuilds: true },
 	
